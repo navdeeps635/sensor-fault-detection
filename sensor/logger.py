48222b9607e0bbcc80ee4c69d0ff7c3ea45f117f
navdeeps635 <navdeep3135@gmail.com>
2022-12-03 15:59:15 +0530

    created logging module and basic

---
+++ sensor/logger.py
@@ -0,0 +1,21 @@
+import logging
+import os
+from datetime import datetime
+
+#log file name
+log_file_name = f"{datetime().now().strftime('%m%d%Y_%H%M_%S')}.log"
+
+#log directory
+log_file_dir = os.path.join(os.getcwd(),"logs")
+
+#create folder if not available
+os.makedirs(log_file_dir,exist_ok=True)
+
+#log file path
+log_file_path = os.path.join(log_file_dir,log_file_name)
+
+logging.basicConfig(
+    filename=log_file_path,
+    format= "[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
